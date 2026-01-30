0b9e83e18f91fbe89f842cf6f758846cfa5995d1
Danish nisar <dk41984200@gmail.com>
2026-01-30 12:26:39 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+"""variables"""
+print("Start our data science journey")
+
+"""data types"""
+
+# numbers
+
+
+a = 12
+b = 12.865
+c = 12/3
+d = 12j # only can use j here not any other like x and y etc etc
+
+print(type(b))
+
+name = "danish nisar" 
+age = 18
+print("Hello my name is",name,"and my age is",age)
+
+# or 
+
+print(f"my name is {name} and my age is {age}")
